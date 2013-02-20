760e71cb856efb75a3f453c93ae130a19209412b
xenor-laptop <xenor@codegreenit.com>
2013-02-20 23:08:03 +0800

    add git shell cmd

---
+++ lib/git-i18n-differ/git.rb
@@ -0,0 +1,10 @@
+class GitI18nDiffer::Git
+  class << self
+    def from_shell(cmd)
+      r = `#{cmd}`
+      raise ArgumentError.new("#{cmd} failed")  unless $?.exitstatus.zero?
+      r.force_encoding(Encoding::UTF_8) if r.respond_to?(:force_encoding)
+      r
+    end
+  end
+end
