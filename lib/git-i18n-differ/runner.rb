84c7740800d7dc86ed8e47a9044d4aeea2785992
xenor <xenor@codegreenit.com>
2013-02-21 17:58:30 +0800

    tweak

--- lib/git-i18n-differ/runner.rb
+++ lib/git-i18n-differ/runner.rb
@@ -1,8 +1,10 @@
 require 'yaml'
-class GitI18nDiffer::Runner
-  class << self
-    def run(config)
-      @config = YAML::load_file(config)
+module GitI18nDiffer
+  class Runner
+    class << self
+      def run(config, oldrev, newrev)
+        @config = YAML::load_file(config)
+      end
     end
   end
 end
