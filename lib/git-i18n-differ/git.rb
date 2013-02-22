61e94f715ecd4e7af08345c139b3d8b1bad1a1b3
xenor <xenor@codegreenit.com>
2013-02-22 17:38:33 +0800

    change the exec filename

--- lib/git-i18n-differ/git.rb
+++ lib/git-i18n-differ/git.rb
@@ -1,32 +1,33 @@
-class GitI18nDiffer::Git
-  class << self
-    def from_shell(cmd)
-      r = `#{cmd}`
-      raise ArgumentError.new("#{cmd} failed") unless $?.exitstatus.zero?
-      r.force_encoding(Encoding::UTF_8) if r.respond_to?(:force_encoding)
-      r
-    end
-
-    def get_diff(file, oldrev, newrev)
-      line = from_shell("git log #{oldrev}..#{newrev} --name-status --pretty=oneline #{file}")
-      status = get_status(line)
-      if status == 'D'
-        Logger.info "file deleted in revision"
-      else
-        gitopt = " --date=rfc2822"
-        gitopt += " --pretty=fuller"
-        #gitopt += " -w" if opts[:ignore_whitespace] == 'all'
-        gitopt += " -b"
-        Logger.info "git show #{rev.strip}#{gitopt}"
-        from_shell("git show #{rev.strip}#{gitopt}")
+module GitI18nDiffer
+  class Git
+    class << self
+      def from_shell(cmd)
+        r = `#{cmd}`
+        raise ArgumentError.new("#{cmd} failed") unless $?.exitstatus.zero?
+        r.force_encoding(Encoding::UTF_8) if r.respond_to?(:force_encoding)
+        r
       end
-    end
 
-    def get_status(line)
-      if line
-        line[0,1]
-      else
-        Logger.info "no changes in file"
+      def get_diff(file, oldrev, newrev)
+        line = from_shell("git log #{oldrev}..#{newrev} --name-status --pretty=oneline #{file}")
+        status = get_status(line)
+        if status == 'D'
+          Logger.error "file deleted in revision"
+        else
+          gitopt = " --date=rfc2822"
+          gitopt += " --pretty=fuller"
+          #gitopt += " -w" if opts[:ignore_whitespace] == 'all'
+          gitopt += " -b"
+          from_shell("git show #{newrev.strip}#{gitopt} #{file}")
+        end
+      end
+
+      def get_status(line)
+        if line
+          line[0,1]
+        else
+          Logger.error "no changes in file"
+        end
       end
     end
   end
--- lib/git_i18n_differ.rb
+++ lib/git_i18n_differ.rb
@@ -1,4 +1,4 @@
-ROOT_DIR = File.expand_path(File.dirname(File.dirname(__FILE__)))
+ROOT_DIR ||= File.expand_path(File.dirname(File.dirname(__FILE__)))
 $LOAD_PATH.unshift(ROOT_DIR)
 
 require "lib/git-i18n-differ/version"
